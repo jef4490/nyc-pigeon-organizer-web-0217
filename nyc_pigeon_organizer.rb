311234a015802413e99ea4ea928558c34deb1d26
Jeffrey Hechler <jeffrey.hechler@gmail.com>
2016-12-13 20:15:05 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ pigeon_list = {}
+ data.each do |attribute, value_hash|
+   value_hash.each do |attribute_keys, pigeon_array|
+     pigeon_array.each do |bird|
+       if pigeon_list[bird] == nil
+         pigeon_list[bird] = {attribute => [attribute_keys.to_s]}
+       elsif pigeon_list[bird][attribute] == nil
+         pigeon_list[bird][attribute] = [attribute_keys.to_s]
+       else
+         array_of_stuff = pigeon_list[bird][attribute]
+         #binding.pry
+         array_of_stuff.push(attribute_keys.to_s)
+       end
+     end
+   end
+ end
+ pigeon_list
 end
